c56a282384b7a259dca7f5027ab5bf2f44e7d092
Grigory Kuimov <g.kuimov@e-mba.ru>
2018-05-29 19:41:11 +0300

    gh fix

--- js/cert.js
+++ js/cert.js
@@ -25,7 +25,7 @@ const elements = [{
 ];
 
 // GETTING DATA
-$.getJSON("json/data.json")
+$.getJSON("/cert-generator/json/data.json")
     .done(function (json) {
         userData = defaultData = json;
     })
