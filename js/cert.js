93280f24802cfb53ba2efc173cbef63e7a4cf081
Grigory Kuimov <g.kuimov@e-mba.ru>
2018-05-31 11:02:16 +0300

    path upd

--- js/cert.js
+++ js/cert.js
@@ -25,7 +25,7 @@ const elements = [{
 ];
 
 // GETTING DATA
-$.getJSON("/cert-generator/json/data.json")
+$.getJSON("https://gk061090.github.io/cert-generator/json/data.json")
     .done(function (json) {
         userData = defaultData = json;
     })
